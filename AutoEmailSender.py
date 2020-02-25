5b35a3956b0e532efd7c42236568979e3c3af7ba
Alizaib Hassan <nsi2019-02-cs14@nxb.com.pk>
2020-02-25 16:51:41 +0500

    Ready for my use

--- AutoEmailSender.py
+++ AutoEmailSender.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import smtplib
 
 
-data = feedparser.parse('http://feeds.feedburner.com/AlizaibHassan')
+data = feedparser.parse('http://feeds.feedburner.com/AlizaibHassan') #your feedburner url
 
 #fetching the title of post
 postTitle = data.entries[0].title
@@ -17,25 +17,12 @@ postLink = postLink.replace('#comment-form', '')
 soup = BeautifulSoup(data.entries[0].summary,"html.parser")
 summary = soup.get_text()
 
-#fetching the article published date
-publishingDate = data.entries[0].published_parsed
-PublishedDay = getattr(publishingDate,'tm_mday')
-PublishedMonth = getattr(publishingDate,'tm_mon')
-PublishedYear = getattr(publishingDate,'tm_year')
-
-#fetching current date and time
-currentDay = datetime.now().day
-currentMonth = datetime.now().month
-currentYear = datetime.now().year
-
 #Sendind Email ...
-gmail_user = 'youremail@gmail.com'
-gmail_password = 'youremailpassword'
-
-#if currentYear == PublishedYear and currentMonth == PublishedMonth and currentDay == PublishedDay:
+gmail_user = 'yourmail@gmail.com'
+gmail_password = 'password'
 
 sent_from = gmail_user
-to = ['sender1@gmail.com', 'sender2@gmail.com']
+to = ['receive1@gmail.com', 'receive2@gmail.com']
 subject = 'Latest Blog Post '+postTitle
 body = 'Title of Article: '+postTitle+'\n\n'+'Article URL: '+postLink+'\n\n'+'Summray: '+summary+'\n'
 
@@ -43,7 +30,6 @@ email_text = """\
 From: %s
 To: %s
 Subject: %s
-
 %s
 """ % (sent_from, ", ".join(to), subject, body)
 
@@ -53,7 +39,6 @@ try:
     server.login(gmail_user, gmail_password)
     server.sendmail(sent_from, to, email_text)
     server.close()
-
     print('Email sent!')
 except:
     print('Something went wrong...')
